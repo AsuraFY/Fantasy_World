0389a5e5706ba489c054e4d740d9775b49acff4d
laroulettebleue <codoeunice40@gmail.com>
2022-02-01 10:50:13 +0000

    fw1

---
+++ src/test/java/com/nice/fantasy_world/FantasyWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nice.fantasy_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
